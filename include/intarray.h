59301430186b606f05001a22692d0752a9ec624d
Martin Nilsson <martin.no.nilsson@gmail.com>
2021-11-28 00:33:59 +0100

    Trying to fix the problems i created when i...

---
+++ include/intarray.h
@@ -0,0 +1,26 @@
+
+#ifndef _intarray_h
+#define _intarray_h
+
+#define CAPACITY 20
+
+typedef int intArray[CAPACITY];
+
+void printArray(intArray a);
+int getIntArraySize(intArray a);
+int getIntArray(intArray a);
+void appendIntArray(intArray a, intArray b, intArray c);
+void interleaveIntArray(intArray a, intArray b, intArray c);
+void sortIntArray(intArray a);
+void setSortOrder(enum SortOrder sortOrder);
+void setUniqueness(enum Uniqueness uniqueness);
+void bubbleSort(int * const a, const size_t size);
+void swap(int *element1Ptr, int *element2Ptr);
+
+/* 
+
+fMatrix code
+
+*/
+
+#endif /*_intarray_h */
